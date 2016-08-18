7b2d3ddf126979f23f872399f764e1c480bef412
Jaron <Jaron780@gmail.com>
2016-08-18 15:32:02 -0500

    Jaron edit

--- src/org/vivecraft/VSE.java
+++ src/org/vivecraft/VSE.java
@@ -1,152 +1,144 @@
 package org.vivecraft;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChannelEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRegisterChannelEvent;
 
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.spigotmc.SpigotConfig;
-import org.vivecraft.command.ViveCommand;
+import org.vivecraft.command.Vive;
 import org.vivecraft.listeners.VivecraftNetworkListener;
 
-import net.milkbowl.vault.permission.Permission;
 
-public class VSE extends JavaPlugin implements Listener {
-	FileConfiguration config = getConfig();
+public class VSE extends JavaPlugin implements Listener{
+
 	public final String CHANNEL = "Vivecraft";
 
-	public Map<UUID, VivePlayer> vivePlayers = new HashMap<UUID, VivePlayer>();
+	public Map<UUID, VivePlayer> vivePlayers = new HashMap<UUID, VivePlayer>(); 
 
-	int task = 0;
-	
-	
 	@Override
 	public void onEnable() {
 		super.onEnable();
-	   //Config Part
-	    config.options().copyDefaults(true);
-	    saveDefaultConfig();
-	    saveConfig();
-	    //end Config part
-	    
-	   //Command Part
-	    this.getCommand("vive").setExecutor(new ViveCommand());
-	    //end Command Part
+		
+		createConfig();
+		this.getCommand("vive").setExecutor(new Vive(this));
 		getServer().getMessenger().registerIncomingPluginChannel(this, CHANNEL, new VivecraftNetworkListener(this));
 		getServer().getMessenger().registerOutgoingPluginChannel(this, CHANNEL);
-
-		//TODO: config.yml entries?
-		SpigotConfig.movedWronglyThreshold = 30;
-		SpigotConfig.movedTooQuicklyMultiplier = 64; 
 		
-		task = getServer().getScheduler().scheduleSyncRepeatingTask(this, new  Runnable(){
-			public void run(){
-				sendPosData();
-			}}, 20, 1);
+
+		SpigotConfig.movedWronglyThreshold = 10;
+		SpigotConfig.movedTooQuicklyMultiplier = 64;
 		
 	}
-		
-	public void sendPosData(){
 
-		for (VivePlayer sendTo: vivePlayers.values()){
+	public class PlayerRegisterChannelEvent extends PlayerChannelEvent {
 
-			if(sendTo == null || sendTo.player == null || !sendTo.player.isOnline()) continue; //dunno y but just in case.
-
-			for (VivePlayer v: vivePlayers.values()){	
-
-				if(v == sendTo || v == null || v.player == null || !v.player.isOnline()) continue; 
-
-				double d = sendTo.player.getLocation().distanceSquared(v.player.getLocation());
-
-				if(d < 256*256){
-					//TODO: optional distance value?
-					sendTo.player.sendPluginMessage(this,CHANNEL, v.getUberPacket());
-				}	
-			}
-		}
-	}
-		
-		
-		
-	//Config Save
-	@Override
-	public void onDisable() {
-		saveConfig();
-		getServer().getScheduler().cancelTask(task);
-		super.onDisable();
+	    public PlayerRegisterChannelEvent(final Player player, final String channel) {
+	    	super(player, channel);
+	        getLogger().info("VIveCraft: "+ channel);
+	    }
 	}
 	
 	@EventHandler
-	public void onPlayerQuit(PlayerQuitEvent event) {
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
 		vivePlayers.remove(event.getPlayer().getUniqueId());
 	}
 
-	@EventHandler
-	public void onPlayerConnect(PlayerJoinEvent event) {
-		final Player p = event.getPlayer();
+    @EventHandler
+    public void onPlayerConnect(PlayerJoinEvent event) {
+    	final Player p = event.getPlayer();
 
-		if (getConfig().getBoolean("vive-only.enabled")) {
-			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-				@Override
-				public void run() {
-					if (p.isOnline() && vivePlayers.containsKey(p.getUniqueId())) {
-						p.kickPlayer(getConfig().getString("vive-only.kickmessage"));
+    	if(getConfig().getBoolean("vive-only.enabled")){
+    		getLogger().info(p.getName() +" : Starting Runnable");
+    		getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+    			@Override
+    			public void run() {
+    				if (p.isOnline() && !vivePlayers.containsKey(p.getUniqueId())) {
+    					p.kickPlayer(getConfig().getString("vive-only.kickmessage"));
+    					getLogger().info(p.getName() + " " + "got kicked for not using the Vive Mod");
+    				}
+    			}
+    		}, getConfig().getInt("vive-only.waittime"));
+    	}
+    }
 
-						getLogger().info(p.getName() + " " + "got kicked for not using the Vive Mod");
+    
+    public void setPermissionsGroup(Player p){
+    	
+        Map<String, Boolean> groups = new HashMap<String, Boolean>();
 
-					}
-				}
-			}, getConfig().getInt("vive-only.waittime"));
-		}
-	}
+        boolean isvive = vivePlayers.containsKey(p.getUniqueId());
+        
+        String g_vive = getConfig().getString("permissions.vivegroup");
+        String g_classic = getConfig().getString("permissions.non-vivegroup");
+        if (g_vive != null)
+            groups.put(g_vive,isvive);
+        if (g_classic != null)
+            groups.put(g_classic, !isvive);
 
-	public void setPermissionsGroup(Player p) {
+        if (isvive) {
+            String g_freemove = getConfig().getString("permissions.freemovegroup");
+            if (g_freemove != null)
+                groups.put(g_freemove, !vivePlayers.get(p.getUniqueId()).isTeleportMode);
+        }
 
-		Map<String, Boolean> groups = new HashMap<String, Boolean>();
+        updatePlayerPermissionGroup(p, groups);
+    	
+    }
+    
+    public void updatePlayerPermissionGroup(Player p, Map<String, Boolean> groups) {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        Permission perm = rsp.getProvider();
+        if (perm != null) {
+            for (Map.Entry<String, Boolean> entry : groups.entrySet()) {
+                if (entry.getValue()) {
+                    if (!perm.playerInGroup(p, entry.getKey()))
+                        perm.playerAddGroup(p, entry.getKey());
+                } else {
+                    if (perm.playerInGroup(p, entry.getKey()))
+                        perm.playerRemoveGroup(p, entry.getKey());
+                }
+            }
+        }
+    }
+    
+    private void createConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("ViveCraft Config not found, creating!");
+                saveDefaultConfig();
+                this.getConfig().set("vive-only.enabled", true);
+                this.getConfig().set("vive-only.kickmessage", "You need to have the Vive Mod installed to join this server!");
+                this.getConfig().set("vive-only.waittime", 20);
+                this.getConfig().set("permissions.vivegroup", "vive.vivegroup");
+                this.getConfig().set("permissions.non-vivegroup", "vive.non-vivegroup");
+                this.getConfig().set("permissions.freemovegroup", "vive.freemovegroup");
+                saveConfig();
+            } else {
+                getLogger().info("ViveCraft Config found, loading!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
 
-		boolean isvive = vivePlayers.containsKey(p.getUniqueId());
+        }
 
-		String g_vive = getConfig().getString("permissions.vivegroup");
-		String g_classic = getConfig().getString("permissions.non-vivegroup");
-		if (g_vive != null)
-			groups.put(g_vive, isvive);
-		if (g_classic != null)
-			groups.put(g_classic, !isvive);
-
-		if (isvive) {
-			String g_freemove = getConfig().getString("permissions.freemovegroup");
-			if (g_freemove != null)
-				groups.put(g_freemove, !vivePlayers.get(p.getUniqueId()).isTeleportMode);
-		}
-
-		updatePlayerPermissionGroup(p, groups);
-
-	}
-
-	public void updatePlayerPermissionGroup(Player p, Map<String, Boolean> groups) {
-		RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
-		Permission perm = rsp.getProvider();
-		if (perm != null) {
-			for (Map.Entry<String, Boolean> entry : groups.entrySet()) {
-				if (entry.getValue()) {
-					if (!perm.playerInGroup(p, entry.getKey()))
-						perm.playerAddGroup(p, entry.getKey());
-				} else {
-					if (perm.playerInGroup(p, entry.getKey()))
-						perm.playerRemoveGroup(p, entry.getKey());
-				}
-			}
-		}
-	}
+    }
+     
 
 }
-
-
--- src/org/vivecraft/listeners/VivecraftNetworkListener.java
+++ src/org/vivecraft/listeners/VivecraftNetworkListener.java
@@ -27,8 +27,7 @@ public class VivecraftNetworkListener implements PluginMessageListener {
 		CONTROLLER1DATA,
 		WORLDSCALE,
 		DRAW,
-		MOVEMODE,
-		UBERPACKET
+		MOVEMODE
 	}
 	
 	@Override
@@ -39,14 +38,14 @@ public class VivecraftNetworkListener implements PluginMessageListener {
 		VivePlayer vp = vse.vivePlayers.get(sender.getUniqueId());
 		
 		PacketDiscriminators disc = PacketDiscriminators.values()[payload[0]];
-		
+		vse.getLogger().info("[ViveCraft]debug: bsPacketRecived: " + disc);
 		if(vp == null && disc.ordinal() > 0) {
 			//how?
 			return;
 		}
 		
 		byte[] data = Arrays.copyOfRange(payload, 1, payload.length);
-		
+		vse.getLogger().info("[ViveCraft]debug: PacketRecived: " + disc);
 		switch (disc){
 		case CONTROLLER0DATA:
 			vp.controller0data = data;
@@ -65,8 +64,12 @@ public class VivecraftNetworkListener implements PluginMessageListener {
 			//only we can use that word.
 			break;
 		case VERSION:
-			vse.vivePlayers.put(sender.getUniqueId(), new VivePlayer(sender));
-			sender.sendPluginMessage(vse, vse.CHANNEL, StringToPayload(PacketDiscriminators.VERSION, vse.getDescription().getFullName()));		
+			vse.vivePlayers.put(sender.getUniqueId(), new VivePlayer());
+			sender.sendMessage("[ViveCraft] Welcome Vive user!");
+			//sender.sendRawMessage("Vivecraft");
+			vse.getLogger().info("[ViveCraft]debug: payload: " + StringToPayload(PacketDiscriminators.VERSION, vse.getName()) + " : " + new String(StringToPayload(PacketDiscriminators.VERSION, vse.getName())));
+			vse.getLogger().info("[ViveCraft]debug: payload2: " + new byte[]{(byte) PacketDiscriminators.REQUESTDATA.ordinal()} + " : " + new String(new byte[]{(byte) PacketDiscriminators.REQUESTDATA.ordinal()}));
+			sender.sendPluginMessage(vse, vse.CHANNEL, StringToPayload(PacketDiscriminators.VERSION, vse.getName()));		
 			sender.sendPluginMessage(vse, vse.CHANNEL, new byte[]{(byte) PacketDiscriminators.REQUESTDATA.ordinal()});
 			break;
 		case WORLDSCALE:
@@ -94,5 +97,4 @@ public class VivecraftNetworkListener implements PluginMessageListener {
 	
 	
 }
-
 
