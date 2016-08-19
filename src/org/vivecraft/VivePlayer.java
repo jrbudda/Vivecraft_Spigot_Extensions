21ed460c766a33ea27269dad800fce9de3546ac0
jrbudda <jrbudda@gmail.com>
2016-08-19 17:16:11 -0400

    add extra byte to hmddata if old client

--- src/org/vivecraft/VivePlayer.java
+++ src/org/vivecraft/VivePlayer.java
@@ -99,6 +99,7 @@ public class VivePlayer {
 					java.nio.ByteBuffer.allocate(8).putLong(player.getUniqueId().getMostSignificantBits()).array());
 			output.write(
 					java.nio.ByteBuffer.allocate(8).putLong(player.getUniqueId().getLeastSignificantBits()).array());
+			if(hmdData.length < 29) output.write(0);
 			output.write(hmdData);
 			output.write(controller0data);
 			output.write(controller1data);
