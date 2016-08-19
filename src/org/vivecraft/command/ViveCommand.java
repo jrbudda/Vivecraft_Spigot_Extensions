1815f6294c8ca7d27f41149a0b23584fa88bba47
Jaron <Jaron780@gmail.com>
2016-08-18 21:57:35 -0500

    Fixed bugs, added Easy method to send messages, added version and help command.

--- src/org/vivecraft/command/ViveCommand.java
+++ src/org/vivecraft/command/ViveCommand.java
@@ -1,58 +1,84 @@
 package org.vivecraft.command;
 
+import java.util.ArrayList;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.vivecraft.VSE;
 
 public class ViveCommand implements CommandExecutor {
 
 	private VSE plugin;
+	private ArrayList<Cmd> commands = new ArrayList<Cmd>();
 
 	public ViveCommand(VSE vse) {
 		plugin = vse;
+		commands.add(new Cmd("vive-only", "Set to true to only allow Vivecraft players to play."));
+		commands.add(new Cmd("waittime",
+				"Ticks to wait before kicking a player. The player's client must send a Vivecraft VERSION info in that time."));
+		commands.add(new Cmd("version", "returns the version of the plugin."));
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender instanceof Player || sender.hasPermission("SpigotVive.vive")) {
+		if (sender instanceof Player){// || sender.hasPermission("SpigotVive.vive")) {
 			Player player = (Player) sender;
 			if (args.length >= 1) {
-				if (args[0].equals("vive-only")) {
+				String command = args[0].toLowerCase();
+				//
+				if (command.equals("vive-only") && sender.isOp()) {
 					if (args.length >= 2) {
-						if (args[1].equals("true")) {
+						if (args[1].toLowerCase().equals("true")) {
 							plugin.getConfig().set("vive-only.enabled", true);
-							player.sendMessage("[ViveCraft] Vive-Only has been enabled.");
-						} else if (args[1].equals("false")) {
+							sendMessage("Vive-Only has been enabled.", player);
+
+						} else if (args[1].toLowerCase().equals("false")) {
 							plugin.getConfig().set("vive-only.enabled", false);
-							player.sendMessage("[ViveCraft] Vive-Only has been disabled.");
+							sendMessage("Vive-Only has been disabled.", player);
 						}
 					} else {
-						player.sendMessage("[ViveCraft] Vive-Only: " + plugin.getConfig().get("vive-only.enabled"));
+						sendMessage("Vive-Only: " + plugin.getConfig().get("vive-only.enabled"), player);
 					}
 					plugin.saveConfig();
 				} else
 				//
-				if (args[0].equals("waittime")) {
+				if (command.equals("waittime") && sender.isOp()) {
 					if (args.length >= 2) {
 						try {
 							plugin.getConfig().set("vive-only.waittime", Integer.parseInt(args[1]));
-							player.sendMessage("[ViveCraft] waittime set to " + Integer.parseInt(args[1]));
+							sendMessage("waittime set to " + Integer.parseInt(args[1]), player);
 						} catch (NumberFormatException e) {
-							player.sendMessage("[ViveCraft] Must use numbers");
+							sendMessage("Must use numbers", player);
 						}
 					} else {
-						player.sendMessage("[ViveCraft] waittime: " + plugin.getConfig().get("vive-only.waittime"));
+						sendMessage("waittime: " + plugin.getConfig().get("vive-only.waittime"), player);
 					}
 					plugin.saveConfig();
+				} else
+				//
+				if (command.equals("version")) {
+					PluginDescriptionFile pdf = plugin.getDescription();
+					String version = pdf.getVersion();
+					sendMessage("Version: " + version, player);
+				} else
+				//	
+				if (command.equals("help")) {
+					for (Cmd cm : commands) {
+						sendMessage(cm.getCommand() + " - " + cm.getDescription(), player);
+					}
 				} else {
-					player.sendMessage("[ViveCraft] Unknown command");
+					sendMessage("Unknown command", player);
 				}
 			}
 		} else if (!(sender instanceof Player))
 			plugin.getLogger().info("Only players can run this command!");
 		return true;
+	}
+
+	public static void sendMessage(String message, Player p) {
+		p.sendMessage("[Vivecraft] " + message);
 	}
 
 }
